9dfc985eb906248ff214335dc5a52f4391a1f7db
diamanto <diamanto2011@gmail.com>
2024-12-25 16:55:07 +0300

    Добавлены столбцы с данными пользователей.

--- services.py
+++ services.py
@@ -1,17 +1,35 @@
 import requests
 import time
-from sqlalchemy import create_engine, Column, String, Integer
+from sqlalchemy import create_engine, Column, String, Integer, Boolean, Float
 from sqlalchemy.orm import declarative_base, sessionmaker
 import argparse
 
 # Определяем базу данных и модель
 Base = declarative_base()
 
+
 class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     username = Column(String, unique=True)
     url = Column(String)
+    avatar_url = Column(String)
+    node_id = Column(String)
+    gravatar_id = Column(String)
+    followers_url = Column(String)
+    following_url = Column(String)
+    gists_url = Column(String)
+    starred_url = Column(String)
+    subscriptions_url = Column(String)
+    organizations_url = Column(String)
+    repos_url = Column(String)
+    events_url = Column(String)
+    received_events_url = Column(String)
+    user_type = Column(String)  # Изменено с 'type' на 'user_type', чтобы избежать конфликта с ключевым словом
+    user_view_type = Column(String)
+    site_admin = Column(Boolean)
+    score = Column(Float)
+
 
 class GitHubUserFetcher:
     def __init__(self, db_url='sqlite:///github_users.db'):
@@ -61,7 +79,26 @@ class GitHubUserFetcher:
         with self.Session() as session:  # Используем контекстный менеджер для сессии
             try:
                 for user in users:
-                    github_user = User(username=user['login'], url=user['html_url'])
+                    github_user = User(
+                        username=user['login'],
+                        url=user['html_url'],
+                        avatar_url=user['avatar_url'],
+                        node_id=user['node_id'],
+                        gravatar_id=user.get('gravatar_id', ''),  # Используем get для безопасного доступа
+                        followers_url=user['followers_url'],
+                        following_url=user['following_url'],
+                        gists_url=user['gists_url'],
+                        starred_url=user['starred_url'],
+                        subscriptions_url=user['subscriptions_url'],
+                        organizations_url=user['organizations_url'],
+                        repos_url=user['repos_url'],
+                        events_url=user['events_url'],
+                        received_events_url=user['received_events_url'],
+                        user_type=user['type'],  # Переименовано на user_type
+                        user_view_type=user.get('user_view_type', ''),  # Используем get для безопасного доступа
+                        site_admin=user.get('site_admin', False),  # Используем get для безопасного доступа
+                        score=user.get('score', 0.0)  # Используем get для безопасного доступа
+                    )
                     # Проверяем, существует ли уже пользователь в базе данных
                     existing_user = session.query(User).filter_by(username=user['login']).first()
                     if existing_user is None:
@@ -80,6 +117,7 @@ class GitHubUserFetcher:
             total_users_count = session.query(User).count()
         return total_users_count
 
+
 def main(query):
     fetcher = GitHubUserFetcher()
     users = fetcher.fetch_github_users(query)
@@ -88,6 +126,7 @@ def main(query):
 
     total_users_count = fetcher.get_total_users_count()
     print(f"Общее количество пользователей в базе данных: {total_users_count}")
+
 
 if __name__ == "__main__":
     # Настройка аргументов командной строки

c10ba2d9bf948d1255756e86e9b79aa9cf6242a2
diamanto <diamanto2011@gmail.com>
2024-12-25 17:42:36 +0300

    Рефакторинг

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from utils import run_github_fetcher, get_command_line_arguments
+from utils import run_github_parser, get_command_line_arguments
 
 
 def main():
     try:
         command_line_arguments = get_command_line_arguments()
-        run_github_fetcher(query=command_line_arguments.query)
+        run_github_parser(query=command_line_arguments.query)
     except SystemExit as e:
         print("Ошибка при обработке аргументов командной строки.")
     except Exception as e:

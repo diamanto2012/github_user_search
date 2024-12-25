49c84923c6529e59052115c905682a945aa99afe
diamanto <diamanto2011@gmail.com>
2024-12-25 16:22:47 +0300

    Добавлена обработка ошибок.

--- main.py
+++ main.py
@@ -2,19 +2,31 @@ import argparse
 from services import GitHubUserFetcher
 
 def main(query):
-    fetcher = GitHubUserFetcher()
-    users = fetcher.fetch_github_users(query)
-    if users:  # Проверяем, есть ли пользователи для сохранения
-        fetcher.save_users_to_db(users)
+    try:
+        fetcher = GitHubUserFetcher()
+    except Exception as e:
+        print(f"Ошибка при инициализации GitHubUserFetcher: {e}")
+        return  # Завершаем выполнение функции в случае ошибки
 
-    total_users_count = fetcher.get_total_users_count()
-    print(f"Общее количество пользователей в базе данных: {total_users_count}")
+    try:
+        users = fetcher.fetch_github_users(query)
+        if users:  # Проверяем, есть ли пользователи для сохранения
+            fetcher.save_users_to_db(users)
+
+        total_users_count = fetcher.get_total_users_count()
+        print(f"Общее количество пользователей в базе данных: {total_users_count}")
+    except Exception as e:
+        print(f"Ошибка во время выполнения основной логики: {e}")
 
 if __name__ == "__main__":
     # Настройка аргументов командной строки
     parser = argparse.ArgumentParser(description='Поиск пользователей GitHub по запросу.')
     parser.add_argument('query', type=str, help='Поисковый запрос (например, "octocat")')
 
-    args = parser.parse_args()
-
-    main(args.query)
+    try:
+        args = parser.parse_args()
+        main(args.query)
+    except SystemExit as e:
+        print("Ошибка при обработке аргументов командной строки.")
+    except Exception as e:
+        print(f"Неизвестная ошибка: {e}")

595748d212af0236b950b6275a6add2d06bb9bc3
diamanto <diamanto2011@gmail.com>
2024-12-25 17:22:02 +0300

    Изменено имя файла

--- db_services.py
+++ db_services.py
@@ -136,27 +136,3 @@ class GitHubUserFetcher:
         with self.Session() as session:  # Используем контекстный менеджер для сессии
             total_users_count = session.query(User).count()
         return total_users_count
-
-
-def main(query):
-    fetcher = GitHubUserFetcher()
-    users = fetcher.fetch_github_users(query)
-    if users:  # Проверяем, есть ли пользователи для сохранения
-        fetcher.save_users_to_db(users)
-
-    total_users_count = fetcher.get_total_users_count()
-    print(f"Общее количество пользователей в базе данных: {total_users_count}")
-
-
-if __name__ == "__main__":
-    # Настройка аргументов командной строки
-    parser = argparse.ArgumentParser(
-        description="Поиск пользователей GitHub по запросу."
-    )
-    parser.add_argument(
-        "query", type=str, help='Поисковый запрос (например, "octocat")'
-    )
-
-    args = parser.parse_args()
-
-    main(args.query)

a7bb20004036876ea096ceb508bd760a661837b9
diamanto <diamanto2011@gmail.com>
2024-12-25 17:03:03 +0300

    Рефакторинг.

--- main.py
+++ main.py
@@ -2,33 +2,37 @@ import argparse
 from services import GitHubUserFetcher
 
 
-def run_parser(query: str):
+def run_github_fetcher(query: str):
     try:
-        fetcher = GitHubUserFetcher()
+        github_fetcher = GitHubUserFetcher()
     except Exception as e:
         print(f"Ошибка при инициализации GitHubUserFetcher: {e}")
         return  # Завершаем выполнение функции в случае ошибки
 
     try:
-        users = fetcher.fetch_github_users(query)
+        users = github_fetcher.fetch_github_users(query)
         if users:  # Проверяем, есть ли пользователи для сохранения
-            fetcher.save_users_to_db(users)
+            github_fetcher.save_users_to_db(users)
 
-        total_users_count = fetcher.get_total_users_count()
+        total_users_count = github_fetcher.get_total_users_count()
         print(f"Общее количество пользователей в базе данных: {total_users_count}")
     except Exception as e:
         print(f"Ошибка во время выполнения основной логики: {e}")
 
 
-if __name__ == "__main__":
+def main():
     # Настройка аргументов командной строки
-    parser = argparse.ArgumentParser(description='Поиск пользователей GitHub по запросу.')
-    parser.add_argument('query', type=str, help='Поисковый запрос (например, "python django react native backend")')
+    argument_command_line_parser = argparse.ArgumentParser(description='Поиск пользователей GitHub по запросу.')
+    argument_command_line_parser.add_argument('query', type=str, help='Поисковый запрос (например, "python django react native backend")')
 
     try:
-        args = parser.parse_args()
-        run_parser(query=args.query)
+        args = argument_command_line_parser.parse_args()
+        run_github_fetcher(query=args.query)
     except SystemExit as e:
         print("Ошибка при обработке аргументов командной строки.")
     except Exception as e:
         print(f"Неизвестная ошибка: {e}")
+
+
+if __name__ == "__main__":
+    main()

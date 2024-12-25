20dc05a6f7609fa8e25c3af83663ba110539a530
diamanto <diamanto2011@gmail.com>
2024-12-25 17:42:53 +0300

    Рефакторинг, добавлены Exceptions.

--- utils.py
+++ utils.py
@@ -1,34 +1,37 @@
 import argparse
 
-from db_services import GitHubUserFetcher
+from db_services import GitHubUserParser
+from exceptions import GitHubUserParserException, ArgumentCommandLineParisngException
 
 
-def run_github_fetcher(query: str):
+def run_github_parser(query: str):
     try:
-        github_fetcher = GitHubUserFetcher()
-    except Exception as e:
-        print(f"Ошибка при инициализации GitHubUserFetcher: {e}")
-        return  # Завершаем выполнение функции в случае ошибки
-
-    try:
-        users = github_fetcher.fetch_github_users(query)
+        github_parser = GitHubUserParser()
+        users = github_parser.fetch_github_users(query)
         if users:  # Проверяем, есть ли пользователи для сохранения
-            github_fetcher.save_users_to_db(users)
+            github_parser.save_users_to_db(users)
 
-        total_users_count = github_fetcher.get_total_users_count()
+        total_users_count = github_parser.get_total_users_count()
         print(f"Общее количество пользователей в базе данных: {total_users_count}")
     except Exception as e:
-        print(f"Ошибка во время выполнения основной логики: {e}")
+        raise GitHubUserParserException(
+            f"Ошибка во время выполнения основной логики: {e}"
+        )
 
 
 def get_command_line_arguments():
-    # Настройка аргументов командной строки
-    argument_command_line_parser = argparse.ArgumentParser(
-        description="Поиск пользователей GitHub по запросу."
-    )
-    argument_command_line_parser.add_argument(
-        "query",
-        help='Поисковый запрос (например, "python django react native backend")',
-    )
-    args = argument_command_line_parser.parse_args()
-    return args
+    try:
+        # Настройка аргументов командной строки
+        argument_command_line_parser = argparse.ArgumentParser(
+            description="Поиск пользователей GitHub по запросу."
+        )
+        argument_command_line_parser.add_argument(
+            "query",
+            help='Поисковый запрос (например, "python django react native backend")',
+        )
+        args = argument_command_line_parser.parse_args()
+        return args
+    except Exception as e:
+        raise ArgumentCommandLineParisngException(
+            "Не удалось спарсить аргументы коммандной строки."
+        )
